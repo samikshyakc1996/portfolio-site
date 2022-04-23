71f3af8ead555c77d9b8c3ee264c60451dad0706
Samikshya K C <sam@Samikshyas-MacBook-Air.local>
2022-04-22 21:28:19 -0400

    added more proejct

--- pages/posts/project.js
+++ pages/posts/project.js
@@ -12,7 +12,7 @@ export default function project(){
             <title>Projects</title>
         </Head>
         <h1 className={styles.pageTitle}>Projects</h1>
-
+        <a href="">
         <Link href="https://samikshyakc1996.github.io/weather-app-project/">
        <div className={styles.projectCard_odd}>
         <Image  src="/images/weather-pic.png"
@@ -24,8 +24,8 @@ export default function project(){
         <p>Weather app. It provides the temperature for the entered cities.</p>
         </div>
         </Link>
-        
-
+        </a>
+        <a href="">
         <Link href="https://samikshyakc1996.github.io/audio-player-project/">
         <div className={styles.projectCard_even}>
         <p>Audio app. There are lists of songs which lets us to play any song of our choice. </p>
@@ -37,6 +37,8 @@ export default function project(){
         />
         </div>
         </Link>
+        </a>
+        <a href="">
         <Link href="https://cool-kheer-e3c3fc.netlify.app/">
         <div className={styles.projectCard_odd}>
         <Image src="/images/recipe-app.png"
@@ -47,6 +49,8 @@ export default function project(){
         <p>Recipe app. This app provides us the recipe information and ingredients for myriads of items.</p>
         </div>
         </Link>
+        </a>
+        <a href="">
         <Link href="https://luminous-lebkuchen-52b841.netlify.app/">
         <div className={styles.projectCard_even}>
         <p> E-commerce site. Here we can add items to bags and proceed to checkout page.
@@ -59,6 +63,8 @@ export default function project(){
        
         </div>
         </Link>
+        </a>
+        <a href="">
         <Link href="https://ronaksakhuja.github.io/wddmphotogallery/login.html">
         <div className={styles.projectCard_odd}>
         <Image src="/images/photostash.png"
@@ -69,7 +75,8 @@ export default function project(){
         <p>Photostash. This is a photo gallery app where user can log in, add images, delete if they want to and store them in a firebase database.</p>
         </div>
         </Link>
-
+        </a>
+        <a href="">
         <Link href="https://samikshyakc1996.github.io/fetch-api/">
         <div className={styles.projectCard_even}>
         <p>BMI-app. This app is to measure your BMI(Body Mass Index). After entering your height and weight, the app displays your BMI.</p>
@@ -82,7 +89,21 @@ export default function project(){
 
         </div>
         </Link>
-
+        </a>
+        <a href="">
+        <Link href="https://slides.com/samikshya/serif">
+        <div className={styles.projectCard_odd}>
+        <Image src="/images/logo.png"
+                width={200}
+                height={160}
+                alt="Home decor app"
+        />
+        <p>This is a home decor business plan. Here, we offer services such as interior designing,
+                maintenance of house appliances.
+        </p>
+        </div>
+        </Link>
+        </a>
         {/* <Script src="/js/main.js"
         strategy="lazyOnLoad"
         /> */}
